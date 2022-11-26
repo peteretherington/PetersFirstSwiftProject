d2c60382811838344fda6e7157b517392e5c10a4
Peter Etherington <peter.etherington@applydigital.co>
2022-11-25 23:13:55 -0500

    Initial Commit

---
+++ PetersFirstSwiftProject/PetersFirstSwiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetersFirstSwiftProjectApp.swift
+//  PetersFirstSwiftProject
+//
+//  Created by Peter Etherington 2 on 2022-11-25.
+//
+
+import SwiftUI
+
+@main
+struct PetersFirstSwiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
